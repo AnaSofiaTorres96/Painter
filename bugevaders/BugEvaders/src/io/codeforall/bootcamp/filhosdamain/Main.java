2d7a4761c4277013e5e3a51718ec8ef7082a53a8
Sofia Torres <anasofiatorresteixeira@gmail.com>
2024-05-07 17:09:09 +0100

    Bug Evaders initial commit

---
+++ bugevaders/BugEvaders/src/io/codeforall/bootcamp/filhosdamain/Main.java
@@ -0,0 +1,14 @@
+
+package io.codeforall.bootcamp.filhosdamain;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Game g = new Game(20);
+        g.run();
+
+    }
+}
